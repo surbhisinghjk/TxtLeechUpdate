2b3db0eb5233d45fcec002a7c12f7f9433175860
Ayush <112577569+Ayus444@users.noreply.github.com>
2024-09-30 00:04:55 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# TXT UPLOADER
+
+# 𝗕𝗢𝗧 𝗠𝗔𝗗𝗘 𝗕𝗬 RAJ ™
+
+
+## DEPLOY TO HEROKU
+
+
+[![Deploy to heroku chacha](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?template=https://github.com/hemendrasahu67/Pay-bot9-raj/tree/main)
---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7440697201:AAFa9zz0CeVwYXdada8lWou2u5kYPYrh5DA")
+    API_ID = int(os.environ["API_ID", 23303247]
+    API_HASH = os.environ["API_HASH", "23623f737dc15708708c65a7297e1510"]
+    AUTH_USERS = "6931140424"
+# 
